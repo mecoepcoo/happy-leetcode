a6ebecee90ac5e33f891a6e2c57b7a4dd596161e
Tianzhen <340349237@qq.com>
2018-07-22 21:09:36 +0800

    1807222108 check-in

---
+++ reverse-integer.js
@@ -0,0 +1,18 @@
+/**
+ * 给定一个 32 位有符号整数，将整数中的数字进行反转。
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  let sign = '+';
+  if (x < 0) {
+    sign = '-';
+  }
+  let num = +(sign + (Math.abs(x) + '').split('').reverse().join(''));
+  return num >= -Math.pow(2, 31) && num <= Math.pow(2, 31) ? num : 0;
+};
+
+let r1 = reverse(123);
+let r2 = reverse(-123);
+let r3 = reverse(120);
+console.log(r1, r2, r3);
---
+++ two-sum.js
@@ -0,0 +1,21 @@
+/**
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+};
+
+var r = twoSum([5, 6, 13, 15], 17);
+console.log(r);
