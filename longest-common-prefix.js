e9cc44f5ee867a86edee8ad009d4b343bdd2be17
tianzhen <340349237@qq.com>
2018-08-20 21:39:25 +0800

    temp

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -7,3 +7,33 @@
 var longestCommonPrefix = function (strs) {
   
 };
+
+class TrieNode {
+  constructor() {
+    this.key = '';
+    this.son = [];
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+      let cur = this.root;
+      for (let i = 0; i < word.length; i++) {
+        let c = word.charCodeAt(i);
+        let node = cur.edges[c];
+        if (node == null) {
+          node = (cur.edges[c] = new TrieNode());
+          node.value = word.charAt(i);
+          node.numPass = 1;
+        } else {
+          node.numPass++;
+        }
+        cur = node;
+      }
+      return true;
+  }
+}
